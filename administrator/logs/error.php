e7898cffda909f862ccb45b0fddcf403950ab9ff
Arif Ahmed Joy <joy_joy@live.com>
2018-07-02 17:37:54 +0600

    changed htaccess file

---
+++ administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2018-07-02 11:34:04 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2018-07-02T11:34:04+00:00	INFO ::1	joomlafailure	Username and password do not match or you do not have an account yet.
---
+++ administrator/logs/joomla_update.php
@@ -0,0 +1,16 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2018-07-02 11:12:24 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2018-07-02T11:12:24+00:00	INFO ::1	update	Update started by user Super User (126). Old version is 3.6.4.
+2018-07-02T11:12:25+00:00	INFO ::1	update	Downloading update file from https://downloads.joomla.org/cms/joomla3/3-6-5/Joomla_3.6.5-Stable-Update_Package.zip.
+2018-07-02T11:13:52+00:00	INFO ::1	update	File Joomla_3.6.5-Stable-Update_Package.zip successfully downloaded.
+2018-07-02T11:13:52+00:00	INFO ::1	update	Starting installation of new version.
+2018-07-02T11:14:23+00:00	INFO ::1	update	Finalising installation.
+2018-07-02T11:14:24+00:00	INFO ::1	update	Deleting removed files and folders.
+2018-07-02T11:14:31+00:00	INFO ::1	update	Cleaning up after installation.
+2018-07-02T11:14:31+00:00	INFO ::1	update	Update to version 3.6.5 is complete.
+2018-07-02T11:15:12+00:00	INFO ::1	update	Update started by user Super User (126). Old version is 3.6.5.
+2018-07-02T11:15:23+00:00	INFO ::1	update	Downloading update file from https://s3-us-west-2.amazonaws.com/joomla-official-downloads/joomladownloads/joomla3/Joomla_3.8.10-Stable-Update_Package.zip?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Credential=AKIAIZ6S3Q3YQHG57ZRA%2F20180702%2Fus-west-2%2Fs3%2Faws4_request&X-Amz-Date=20180702T111518Z&X-Amz-Expires=60&X-Amz-SignedHeaders=host&X-Amz-Signature=7c0a46364287d8de356a1cea410dcc7cf4212de8b64d2346657630d37e85ffa9.
